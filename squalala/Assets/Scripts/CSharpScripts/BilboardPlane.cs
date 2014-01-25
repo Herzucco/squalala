65e16e90e9ac1bd49c17afa9affe5eb0937ded7c
Adrien Carta <adrien.carta@laposte.net>
2014-01-25 05:22:55 +0100

    add bilboard

---
+++ squalala/Assets/Scripts/CSharpScripts/BilboardPlane.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BilboardPlane : MonoBehaviour {
+	
+	public Camera Cam;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 v = Cam.transform.forward;
+		v.y += 90;
+		transform.rotation = Quaternion.LookRotation(v);
+	}
+}
