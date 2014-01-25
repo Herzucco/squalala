0bb43bfc8fdcceb64985ad0b4c42a063324dbca5
Adrien Carta <adrien.carta@laposte.net>
2014-01-25 03:13:08 +0100

    save work

---
+++ squalala/Assets/Scripts/CSharpScripts/SplitScreenManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplitScreenManager : MonoBehaviour {
+	public Camera leftCamera;
+	public Camera rightCamera;
+	
+	private bool moving;
+	private float screenPosition;
+	private float speed;
+	// Use this for initialization
+	void Start () {
+		this.MoveToIn(0.2f, -0.1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.moving){
+			this.Move();
+		}
+	}
+
+	public void MoveToIn(float screenPosition, float speed){
+		this.speed = speed;
+		this.moving = true;
+		this.screenPosition = screenPosition;
+	}
+
+	public float screenX{
+		get{
+			return this.camera.rect.x;
+		}
+	}
+
+	void Move(){
+		Rect leftRect = this.leftCamera.rect;
+		Rect rightRect = this.rightCamera.rect;
+		Rect splitter = this.camera.rect;
+		float lastXSplitter = splitter.x;
+
+		leftRect.width += this.speed * Time.deltaTime;
+		rightRect.x += this.speed * Time.deltaTime;
+		rightRect.width -= this.speed * Time.deltaTime;
+		splitter.x += this.speed * Time.deltaTime;
+
+		this.leftCamera.rect = leftRect;
+		this.rightCamera.rect = rightRect;
+		this.camera.rect = splitter;
+
+		if(this.camera.rect.x <= lastXSplitter){
+			if(this.camera.rect.x <= this.screenPosition){
+				this.moving = false;
+				return;
+			}
+		}
+		else{
+			if(this.camera.rect.x >= this.screenPosition){
+				this.moving = false;
+				return;
+			}
+		}
+	}
+}
