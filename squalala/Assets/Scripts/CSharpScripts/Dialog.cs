7062bb1c92b0910c6bc1c55106a19121f5752454
Adrien Carta <adrien.carta@laposte.net>
2014-01-25 09:35:27 +0100

    saving befor checkout

---
+++ squalala/Assets/Scripts/CSharpScripts/Dialog.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialog : MonoBehaviour {
+
+	public string sentence;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ squalala/Assets/Scripts/CSharpScripts/xmlTextBuilder.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System;
+
+public class XmlTextBuilder : MonoBehaviour {
+	public string filePath;
+
+	private XmlDocument xmlDoc;
+	// Use this for initialization
+	void Start () {
+		xmlDoc = new XmlDocument();
+		System.IO.StreamReader file = new System.IO.StreamReader("Assets/Dialogues/" + filePath +".xml");
+		xmlDoc.LoadXml(file.ReadToEnd());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ReadXML() {
+		Dialog dialog;
+		foreach (XmlNode node in xmlDoc.SelectNodes("dialog") )
+		{
+			dialog = new Dialog();
+			dialog.sentence = node.SelectSingleNode("sentence").InnerText;
+			
+			LoadBook(book);
+		}
+	}
+}
