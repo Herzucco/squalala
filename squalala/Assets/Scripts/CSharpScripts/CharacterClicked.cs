263c1ae23c7d9052b28ccaff08e04aae57515003
TagliatL <maurer.paul@ymail.com>
2014-01-25 18:25:36 +0100

    le nGUI par remi, ca bug olala

--- squalala/Assets/Scripts/CSharpScripts/CharacterClicked.cs
+++ squalala/Assets/Scripts/CSharpScripts/CharacterClicked.cs
@@ -2,85 +2,32 @@
 using System.Collections;
 
 public class CharacterClicked : MonoBehaviour {
-
+	
 	public GUIText dialogueSys;
-	public GUIText answers1;
-	public GUIText answers2;
-	public GUIText answers3;
+	
 	public DialogEvent jeanEvent;
 	public CharacterLink link;
-
+	public DialogDisplay dialScript;
 	private int dialPosition;
-	bool dialLoaded;
 	bool displayTheDial;
-
-	public ArrayList dialogueOfTheCharacter;
-	public ArrayList answerToTheCharacter;
-
-	void Start(){
-		dialogueOfTheCharacter = new ArrayList();
-		answerToTheCharacter = new ArrayList();
-		dialPosition = 0;
-		string line = "";
-		string ans = "";
-		System.IO.StreamReader file = new System.IO.StreamReader("Assets/Dialogues/" + this.name +"-questions.txt");
-		while ((line = file.ReadLine()) != null)
-		{
-			dialogueOfTheCharacter.Add(line);	
-		}
-		dialLoaded = true;
-		System.IO.StreamReader file2 = new System.IO.StreamReader("Assets/Dialogues/" + this.name +"-reponses.txt");
-		while ((ans = file2.ReadLine()) != null)
-		{
-			answerToTheCharacter.Add(ans);	
-		}
-		file.Close();
-	}
+	
 	void OnMouseOver(){
-			if(Input.GetMouseButtonDown(0)){
-				displayTheDial = true;
-				jeanEvent.Action();
-			}
-			if(Input.GetMouseButtonDown(1)){
-				displayTheDial = false;
-				link.SendToOtherWorld();
-			}
-	}
-
-	void Update(){
-		if(displayTheDial == true)
-		{
-			if(dialPosition < dialogueOfTheCharacter.Count)
-			{
-				if(Input.GetKeyDown("space"))
-				{
-					dialPosition += 1;
-				}
-				if(dialLoaded == true)
-				{
-					if(((string) dialogueOfTheCharacter[dialPosition]).Contains("??"))
-					{
-						answers1.text = (string) answerToTheCharacter[0];
-						answers2.text = (string) answerToTheCharacter[1];
-						answers3.text = (string) answerToTheCharacter[2];
-					}
-
-					dialogueSys.text = (string) dialogueOfTheCharacter[dialPosition];
-				}
-			}
-			else
-			{
-				dialogueSys.text ="";
-			}
+		if(Input.GetMouseButtonDown(0) && displayTheDial == false){
+			displayTheDial = true;
+			jeanEvent.Action();
+			dialScript.talkTo();
 		}
-	}			
-
-
+		if(Input.GetMouseButtonDown(1)){
+			displayTheDial = false;
+			link.SendToOtherWorld();
+		}
+	}
+	
 	void OnMouseEnter(){
 		//save the original material color
 		this.transform.renderer.material.color = Color.cyan;
 	}
-
+	
 	void OnMouseExit(){
 		this.transform.renderer.material.color = Color.white;
 	}
--- squalala/Assets/Scripts/CSharpScripts/DialogDisplay.cs
+++ squalala/Assets/Scripts/CSharpScripts/DialogDisplay.cs
@@ -7,21 +7,66 @@ public class DialogDisplay : MonoBehaviour {
 
 	private int currentDialog;
 	private int currentBranch;
+	public bool isTalking;
+	public GUIText answer1;
+	public GUIText answer2;
+	public GUIText answer3;
+	public GameObject guiRoot;
+	public DynamicButton answerPrefab;
+
 	// Use this for initialization
 	void Start () {
 		currentBranch = 0;
 		currentDialog = 0;
 	}
 	
+	public void talkTo() {
+		isTalking = true;
+ 	}
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown("space"))
-		{
-			currentDialog += 1;
-		}
-		ArrayList branch = (ArrayList)dialogParser.branchs[currentBranch];
-		Dialog dialog = (Dialog) branch[currentDialog];
-		sentence.text = dialog.name + " : " + dialog.sentence;
+
+	void MethodeDePaul(string dial, int currentBranch, int indexInScreen) 
+	{
+		DynamicButton b = Instantiate(answerPrefab, Vector3.zero, Quaternion.identity) as DynamicButton;
+		b.transform.parent = guiRoot.transform;
+		b.transform.localScale = Vector3.one;
+		b.receiver = this.gameObject;
+		b.label = dial;
+		b.transform.localPosition = new Vector3(0, indexInScreen * 35, 0);
 	}
-	
+
+	bool waitAnswer = false;
+
+	void Update () {
+		//if(isTalking == true)
+		{
+			if (waitAnswer) return;
+			if(Input.GetKeyDown("space"))
+			{
+				currentDialog += 1;
+			}
+
+			ArrayList branch = (ArrayList)dialogParser.branchs[currentBranch];
+			if (currentDialog >= branch.Count)
+				return;// fin du dialogue
+
+			Dialog dialog = (Dialog) branch[currentDialog];
+
+			sentence.text = dialog.name + " : " + dialog.sentence;
+
+
+			if(dialog.responses.Count > 0)
+			{
+				waitAnswer = true;
+				for (int i = 1; i < dialog.responses.Count; i++)
+				{
+					MethodeDePaul(((string)dialog.responses[i]), currentBranch+i+1, i);
+				}
+			}
+		}
+	}
+
+
+
+
 }
