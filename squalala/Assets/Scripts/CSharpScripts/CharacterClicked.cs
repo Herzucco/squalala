adaa906a35fa8e58f01c4c7700ba9034c7fdea65
TagliatL <maurer.paul@ymail.com>
2014-01-25 06:26:27 +0100

    improvement of the dialogue system

--- squalala/Assets/Scripts/CSharpScripts/CharacterClicked.cs
+++ squalala/Assets/Scripts/CSharpScripts/CharacterClicked.cs
@@ -3,21 +3,71 @@ using System.Collections;
 
 public class CharacterClicked : MonoBehaviour {
 
-	public GUIText DialogueSys;
+	public GUIText dialogueSys;
+	public GUIText answers1;
+	public GUIText answers2;
+	public GUIText answers3;
 
+	private int dialPosition;
+	bool dialLoaded;
+	bool displayTheDial;
 
-	void OnMouseOver(){
-		if(Input.GetMouseButtonDown(0)){
-			string line = "";
-			System.IO.StreamReader file = new System.IO.StreamReader("Assets/Dialogues/" + this.name +".txt");
-			while ((line = file.ReadLine()) != null)
-			{
-				DialogueSys.text = line;
-			}
-			
-			file.Close();
+	public ArrayList dialogueOfTheCharacter;
+	public ArrayList answerToTheCharacter;
+
+	void Start(){
+		dialogueOfTheCharacter = new ArrayList();
+		answerToTheCharacter = new ArrayList();
+		dialPosition = 0;
+		string line = "";
+		string ans = "";
+		System.IO.StreamReader file = new System.IO.StreamReader("Assets/Dialogues/" + this.name +"-questions.txt");
+		while ((line = file.ReadLine()) != null)
+		{
+			dialogueOfTheCharacter.Add(line);	
 		}
+		dialLoaded = true;
+		System.IO.StreamReader file2 = new System.IO.StreamReader("Assets/Dialogues/" + this.name +"-reponses.txt");
+		while ((ans = file2.ReadLine()) != null)
+		{
+			answerToTheCharacter.Add(ans);	
+		}
+		file.Close();
 	}
+	void OnMouseOver(){
+			if(Input.GetMouseButtonDown(0)){
+				displayTheDial = true;
+			}
+	}
+
+	void Update(){
+		if(displayTheDial == true)
+		{
+			if(dialPosition < dialogueOfTheCharacter.Count)
+			{
+				if(Input.GetKeyDown("space"))
+				{
+					dialPosition += 1;
+				}
+				if(dialLoaded == true)
+				{
+					if(((string) dialogueOfTheCharacter[dialPosition]).Contains("??"))
+					{
+						answers1.text = (string) answerToTheCharacter[0];
+						answers2.text = (string) answerToTheCharacter[1];
+						answers3.text = (string) answerToTheCharacter[2];
+					}
+
+					dialogueSys.text = (string) dialogueOfTheCharacter[dialPosition];
+				}
+			}
+			else
+			{
+				dialogueSys.text ="";
+			}
+		}
+	}			
+
 
 	void OnMouseEnter(){
 		//save the original material color
