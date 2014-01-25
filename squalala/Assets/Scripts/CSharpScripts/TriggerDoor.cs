12b2f19d09178a8ede0f56ca3f04cfb2f9b3b08e
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2014-01-25 08:36:48 +0100

    can grab items and throw them, and can open door if you have the right item

---
+++ squalala/Assets/Scripts/CSharpScripts/TriggerDoor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDoor : MonoBehaviour {
+	
+	public GameObject Parent;
+	public string neededObject;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<Inventory>().objectEquiped == neededObject)
+		{
+			Destroy(Parent);
+		}
+	}
+}
