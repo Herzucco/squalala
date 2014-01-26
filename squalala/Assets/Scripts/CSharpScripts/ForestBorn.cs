3a32013b5a44cc3c120946d9d65beb51e9a1c2f0
TagliatL <maurer.paul@ymail.com>
2014-01-26 13:14:04 +0100

    add some cool stuff in the LD

---
+++ squalala/Assets/Scripts/CSharpScripts/ForestBorn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForestBorn : MonoBehaviour {
+
+	public GameObject bridge;
+	public GameObject forestToMove;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Plant")
+		{
+			iTween.MoveBy(forestToMove, iTween.Hash("y", 5, "easeType", "easeInOutExpo", "delay", 0, "time", 3));
+			iTween.MoveBy(bridge, iTween.Hash("z", -18, "easeType", "easeInOutExpo", "delay", 0, "time", 3));
+
+		}
+	}
+}
