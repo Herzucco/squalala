bf5ea7d870f55b7dd5980629259ec1f55fa11622
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2014-01-26 16:42:17 +0100

    LA VIDEOOOOO

---
+++ squalala/Assets/Scripts/CSharpScripts/MoviePlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlayer : MonoBehaviour {
+
+	public MovieTexture endVideo;
+	public GameObject movieCamera;
+	public GameObject fpsCamera;
+	public GameObject pointAndClickCamera;
+	public GameObject cible;
+
+	// Use this for initialization
+	void Start () {
+		LaunchVideo();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void LaunchVideo() {
+		movieCamera.active = true;
+		fpsCamera.active = false;
+		pointAndClickCamera.active = false;
+		cible.renderer.material.mainTexture = endVideo;
+		endVideo.Play();
+	}
+}
