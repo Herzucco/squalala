87835e47849d88ae819c38ffb2e3833a6d84891f
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2014-01-27 19:17:19 +0100

    Add Menu and end Video

---
+++ squalala/Assets/Scripts/CSharpScripts/PressStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressStart : MonoBehaviour {
+
+	public string levelName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1") || Input.GetButton("Fire2") || Input.GetButton("Jump") || Input.GetButton("Grab") || Input.GetButton("Throw") || Input.GetButton("Kill"))
+		{
+			Application.LoadLevel(levelName);
+		}
+	}
+}
---
+++ squalala/Assets/Video/PlayVideo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideo : MonoBehaviour {
+
+	public MovieTexture movTexture;
+	public AudioSource movSound;
+	public string levelName;
+
+	void Start() {
+		renderer.material.mainTexture = movTexture;
+		movTexture.Play();
+		movSound.Play();
+	}
+
+	void Update() {
+		if (movTexture.isPlaying == false)
+		{
+			Application.LoadLevel(levelName);
+		}
+	}
+
+}
